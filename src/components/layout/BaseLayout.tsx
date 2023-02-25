0f420964c4d7305a1dc4bf49391c0838b38f5a61
SalvatoreLa <salvatore.la.spata@gmail.com>
2023-02-25 18:36:41 +0100

    update layout

--- src/components/layout/BaseLayout.tsx
+++ src/components/layout/BaseLayout.tsx
@@ -3,14 +3,14 @@ import { Header } from './Header';
 
 const BaseLayout: React.FC<{ title: string, children: React.ReactNode }> = ({ title, children }) => {
     return (
-        <>
+        <div className="App">
             <Header />
             <main>
                 <h1 className='px-4'><span className='text-gradient'>{title}</span></h1>
                 {children}
             </main>
             <Footer />
-        </>
+        </div>
     )
 }
 
