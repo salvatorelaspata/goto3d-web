a29d421ac146c1aeaf1802eca1c84b9cd1a8d683
salvatorelaspata <salvatore.la.spata@gmail.com>
2023-11-18 15:12:52 +0100

    working

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -8,18 +8,21 @@ import { useRouter } from "next/router"
 const Project: React.FC<{
   _project: Database['public']['Tables']['Project']['Row'],
   _process: Database['public']['Tables']['Process']['Row'],
-  models: any,
-  objUrl: string
-}> = ({ objUrl }) => {
+  objUrl: string,
+  textureUrl: string,
+  backgroundUrl: string
+}> = ({ objUrl, textureUrl, backgroundUrl }) => {
   const { query: { id } } = useRouter()
   return (
-    <BaseLayout title="Project">
-      <h1>Project {id}</h1>
-      <Viewer objectUrl={objUrl} />
+    <BaseLayout title={`Project ${id}`}>
+      <Viewer
+        objUrl={objUrl}
+        textureUrl={textureUrl}
+        backgroundUrl={backgroundUrl}
+      />
     </BaseLayout>
   )
 }
-
 
 export default Project
 
@@ -32,14 +35,22 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       supabase.from('Project').select('id, status, name, description, file_location').eq('id', id).single(),
       supabase.from('Process').select('models_url, detail, order, feature, created_at, finished_at').eq('project_id', id).single(),
     ])
-    // get list of models in a folder (file_location)
+    // get list of models in a folder (file_location) & list of backgrounds
     const { data: models } = await supabase.storage.from('viewer3d-dev').list(`${_project?.file_location}`)
-    // get the obj file name
+    const { data: backgrounds } = await supabase.storage.from('viewer3d-dev').list('HDR')
+
+    // get the obj file name, the texture file name and a random background
     const objName: string = models?.find((m) => m.name.endsWith('.obj'))?.name || ''
+    const textureName: string = models?.find((m) => m.name === 'baked_mesh_tex0.png')?.name || ''
+    const backgroundName: string = backgrounds && backgrounds.length > 0 ? backgrounds[Math.floor(Math.random() * backgrounds.length)]?.name : ''
+
     // get the signed url for the obj file
-    const { data } = await supabase.storage.from('viewer3d-dev').createSignedUrl(`${_project?.file_location}${objName}`, 60)
+    const { data: objUrl } = await supabase.storage.from('viewer3d-dev').createSignedUrl(`${_project?.file_location}${objName}`, 60)
+    const { data: textureUrl } = await supabase.storage.from('viewer3d-dev').createSignedUrl(`${_project?.file_location}${textureName}`, 60)
+    const { data: backgroundUrl } = await supabase.storage.from('viewer3d-dev').createSignedUrl(`HDR/${backgroundName}`, 60)
+
     return {
-      props: { _project, _process, models, objUrl: data?.signedUrl }
+      props: { _project, _process, models, objUrl: objUrl?.signedUrl, textureUrl: textureUrl?.signedUrl, backgroundUrl: backgroundUrl?.signedUrl }
     }
   } catch (error) {
     return {
