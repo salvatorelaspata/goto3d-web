971bb85c5a197ed0e343c0cc3a52fae8d636cb35
SalvatoreLa <salvatore.la.spata@gmail.com>
2024-06-05 19:04:53 +0200

    fix build

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,93 +1,9 @@
-// import { Viewer } from "@/components/Viewer";
-import { createClient } from "@/utils/supabase/server";
-import { Database } from "@/types/supabase";
 import { protectedRoute } from "@/app/projects/actions";
 import { Viewer3d } from "@/components/Viewer3d";
 import PageTitle from "@/components/PageTitle";
 import { formatSupabaseDate } from "@/utils/constants";
 import Image from "next/image";
-
-type ProjectDetail = {
-  project: Database["public"]["Tables"]["project"]["Row"];
-  objUrl: string;
-  textureUrl: string;
-  backgroundUrl: string;
-};
-
-const fetchData: ({
-  id,
-}: {
-  id: string;
-}) => Promise<ProjectDetail | undefined> = async ({ id }) => {
-  const _id: number = parseInt(id as string);
-  const supabase = createClient();
-  try {
-    const { data: project } = await supabase
-      .from("project")
-      .select("*")
-      .eq("id", _id)
-      .single();
-
-    const { data: models } = await supabase.storage
-      .from("viewer3d-dev")
-      .list(`${project?.id}/model`);
-
-    // console.log("models", project?.id, models);
-
-    const { data: backgrounds } = await supabase.storage
-      .from("viewer3d-dev")
-      .list("HDR");
-
-    // get the obj file name, the texture file name and a random background
-    const objName: string =
-      models?.find((m) => m.name.endsWith(".obj"))?.name || "";
-    const textureName: string =
-      models?.find((m) => m.name === "baked_mesh_tex0.pngbaked_mesh_tex0.png")
-        ?.name || "";
-    const backgroundName: string =
-      backgrounds && backgrounds.length > 0
-        ? backgrounds[Math.floor(Math.random() * backgrounds.length)]?.name
-        : "";
-    let objUrl: string | undefined = "";
-    let textureUrl: string | undefined = "";
-    let backgroundUrl: string | undefined = "";
-    // get the signed url for the obj file
-    try {
-      // obj
-      const { data: _objUrl, error: _objError } = await supabase.storage
-        .from("viewer3d-dev")
-        .createSignedUrl(`${project?.id}/model/${objName}`, 20);
-
-      objUrl = _objUrl?.signedUrl;
-      // texture
-      const { data: _textureUrl, error: _textureError } = await supabase.storage
-        .from("viewer3d-dev")
-        .createSignedUrl(`${project?.id}/model/${textureName}`, 20);
-
-      textureUrl = _textureUrl?.signedUrl;
-
-      const { data: _backgroundUrl, error: _backgroundError } =
-        await supabase.storage
-          .from("viewer3d-dev")
-          .createSignedUrl(`HDR/${backgroundName}`, 20);
-
-      backgroundUrl = _backgroundUrl?.signedUrl;
-    } catch (error) {
-      console.error("error supabase", error);
-    }
-
-    console.log(objUrl, textureUrl, backgroundUrl);
-
-    return {
-      project,
-      objUrl: objUrl || "",
-      textureUrl: textureUrl || "",
-      backgroundUrl: backgroundUrl || "",
-    };
-  } catch (error) {
-    console.error("error", error);
-  }
-};
+import { fetchData } from "./actions";
 
 export default async function Project({ params }: { params: { id: string } }) {
   await protectedRoute();
@@ -100,8 +16,8 @@ export default async function Project({ params }: { params: { id: string } }) {
       </div>
     );
   };
-  if (!p) return bigTextCentered("loading...");
-  const { status } = p?.project;
+  if (!p || !p.project) return bigTextCentered("loading...");
+  const status = p?.project.status;
   if (status === "error") {
     return bigTextCentered("Errore");
   } else if (status === "in queue") {
