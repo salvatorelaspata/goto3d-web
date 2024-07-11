ca26a774e3b422727ff56217c8510d03122e537e
SalvatoreLa <salvatore.la.spata@gmail.com>
2024-07-11 16:42:36 +0200

    card catalog

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,15 +1,16 @@
 import PageTitle from "@/components/ui/PageTitle";
-import { getProjects, protectedRoute } from "./actions";
 import { Projects } from "@/components/projects/Projects";
+import { protectedRoute } from "../actions";
+import { getProjects } from "./actions";
 
 export default async function Project() {
   await protectedRoute();
   const projects = await getProjects();
   return (
     <>
-      <section className="m-4 bg-palette2 rounded-lg h-[77vh] bg-gradient-to-b from-[#006D77] to-[#83C5BE] flex items-center justify-center">
-        <div className="max-w-2xl px-4 md:px-6 text-center space-y-6 text-palette3">
-          <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+      <section className="m-4 flex h-[77vh] items-center justify-center rounded-lg bg-palette2 bg-gradient-to-b from-[#006D77] to-[#83C5BE]">
+        <div className="max-w-2xl space-y-6 px-4 text-center text-palette3 md:px-6">
+          <h1 className="text-4xl font-bold tracking-tight md:text-6xl">
             Crea il tuo progetto 3D
           </h1>
           <p className="text-lg md:text-xl">
@@ -18,14 +19,14 @@ export default async function Project() {
           <form action="/projects/new">
             <button
               type="submit"
-              className="w-full max-w-2xl bg-palette3 p-6 rounded-sm text-palette1 hover:bg-palette1 hover:text-palette3 text-3xl"
+              className="w-full max-w-2xl rounded-sm bg-palette3 p-6 text-3xl text-palette1 hover:bg-palette1 hover:text-palette3"
             >
               CREA
             </button>
           </form>
         </div>
       </section>
-      <section className="m-4 bg-palette2 rounded-lg">
+      <section className="m-4 rounded-lg bg-palette2">
         <PageTitle title="I Tuoi Progetti" />
         <Projects projects={projects} />
       </section>
--- components/catalogs/Card.tsx
+++ components/catalogs/Card.tsx
@@ -23,10 +23,10 @@ export default function Card({
   }
 
   return (
-    <div className="group relative cursor-pointer rounded-md bg-palette3 p-3 shadow-lg transition duration-300 ease-in-out hover:scale-105 hover:bg-palette1 hover:shadow-2xl">
+    <div className="group relative m-3 cursor-pointer rounded-md bg-palette3 shadow-lg transition duration-300 ease-in-out hover:scale-105 hover:bg-palette1 hover:shadow-2xl">
       <Link
         key={id}
-        className="m-3 flex flex-col overflow-hidden p-3"
+        className="flex flex-col overflow-hidden p-3"
         href={`/catalogs/${id}`}
       >
         <div className="flex justify-between">
@@ -42,19 +42,15 @@ export default function Card({
         </p>
       </Link>
       {artifact && (
-        <form className="z-30" action={navigate}>
+        <form className="z-30 m-2" action={navigate}>
           <button
             type="submit"
-            className="w-full cursor-pointer text-right text-xl font-bold text-palette1 hover:text-palette5 group-hover:text-palette3"
+            className="w-full cursor-pointer rounded-md bg-palette3 px-2 text-right text-xl font-bold text-palette1 hover:text-palette5"
           >
             Visualizza
           </button>
         </form>
       )}
-      <span className="absolute top-0 left-0 h-0 w-1 bg-palette3 group-hover:h-full group-hover:transition-all"></span>
-      <span className="absolute bottom-0 right-0 h-0 w-1 bg-palette3 group-hover:h-full group-hover:transition-all"></span>
-      <span className="absolute bottom-0 left-0 h-1 w-0 bg-palette3 group-hover:w-full group-hover:transition-all"></span>
-      <span className="absolute top-0 right-0 h-1 w-0 bg-palette3 group-hover:w-full group-hover:transition-all"></span>
     </div>
   );
 }
