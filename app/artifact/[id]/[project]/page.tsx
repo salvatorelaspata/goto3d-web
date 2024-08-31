662b9de4f370d82bc755c490501a116af7e35102
SalvatoreLa <salvatore.la.spata@gmail.com>
2024-08-31 14:30:48 +0200

    fix build & test img

--- app/artifact/[id]/[project]/page.tsx
+++ app/artifact/[id]/[project]/page.tsx
@@ -1,4 +1,4 @@
-import { Viewer3d } from "@/components/viewer3d/Viewer3d";
+// import { Viewer3d } from "@/components/viewer3d/Viewer3d";
 import { fetchData } from "@/app/projects/[id]/actions";
 
 export default async function Artifact({
@@ -11,10 +11,10 @@ export default async function Artifact({
   return (
     <section className="h-screen p-4">
       <div className="relative flex h-full w-full flex-col items-center justify-center rounded-lg bg-palette2 bg-gradient-to-b from-[#006D77] to-[#83C5BE]">
-        <h1 className="absolute top-4 left-4 text-lg font-bold tracking-tight md:text-6xl">
+        <h1 className="absolute left-4 top-4 text-lg font-bold tracking-tight md:text-6xl">
           {p.project.name}
         </h1>
-        <Viewer3d id={p.project.id} objectUrl="" textureUrl="" />
+        {/* <Viewer3d id={p.project.id} objectUrl="" textureUrl="" /> */}
       </div>
     </section>
   );
